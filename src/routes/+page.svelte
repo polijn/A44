012b7c643577880f15189cc8df81d7fa6cca3d45
polijn <leander.manz@gmail.com>
2025-02-28 17:13:30 +0100

    update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,21 @@
 <script>
 	import AudioAnalyzer from '$lib/AudioAnalyzer.svelte';
 	import Metronome from '$lib/Metronme.svelte';
+	import MetronomeV2 from '$lib/MetronomeV2.svelte';
 </script>
 
 <main>
-	<AudioAnalyzer />
-	<Metronome />
+	<div class="mt-4">
+		<AudioAnalyzer />
+	</div>
+
+	<div class="mt-4">
+		<MetronomeV2 />
+	</div>
+	<div class="mt-4">
+		<Metronome />
+	</div>
+	<!-- <div class="mt-4">
+
+	</div> -->
 </main>
