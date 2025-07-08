ba8db2d1f22c4f4168f96bc123709f37f7a7723c
polijn <45722770+polijn@users.noreply.github.com>
2025-07-08 11:32:38 +0200

    Update +page.svelte

--- src/routes/testing/+page.svelte
+++ src/routes/testing/+page.svelte
@@ -1,12 +1,3 @@
-<script
-  src="https://unpkg.com/@vapi-ai/client-sdk-react/dist/embed/widget.umd.js"
-  async
-  type="text/javascript"
->
-</script>
+<vapi-widget mode="voice" theme="light" base-color="#ffffff" accent-color="#14B8A6" button-base-color="#f40707" button-accent-color="#ffffff" radius="large" size="full" position="top-left" main-label="TALK WITH AI" start-button-text="Start" end-button-text="End Call" require-consent="true" local-storage-key="vapi_widget_consent" show-transcript="true" public-key="77df2139-6329-4f96-9b08-4394fbfb865f" assistant-id="07e86b06-989e-40da-b09f-acde9377d6b5"></vapi-widget>
 
-
-Hello there: 
-
-
-<vapi-widget assistant-id="07e86b06-989e-40da-b09f-acde9377d6b5" public-key="77df2139-6329-4f96-9b08-4394fbfb865f"></vapi-widget>
+<script src="https://unpkg.com/@vapi-ai/client-sdk-react/dist/embed/widget.umd.js" async type="text/javascript"></script>
