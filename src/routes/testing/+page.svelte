feecf479b112fb334038d8e247d28ad97ed5bba1
polijn <45722770+polijn@users.noreply.github.com>
2025-07-08 11:43:36 +0200

    Update +page.svelte

--- src/routes/testing/+page.svelte
+++ src/routes/testing/+page.svelte
@@ -11,6 +11,7 @@
   });
 </script>
 
+<!-- Voice Widget -->
 <vapi-widget
   mode="voice"
   theme="light"
@@ -22,6 +23,27 @@
   size="full"
   position="top-left"
   main-label="TALK WITH AI"
+  start-button-text="Start"
+  end-button-text="End Call"
+  require-consent="true"
+  local-storage-key="vapi_widget_consent"
+  show-transcript="true"
+  public-key="77df2139-6329-4f96-9b08-4394fbfb865f"
+  assistant-id="07e86b06-989e-40da-b09f-acde9377d6b5">
+</vapi-widget>
+
+<!-- Chat Widget -->
+<vapi-widget
+  mode="chat"
+  theme="light"
+  base-color="#ffffff"
+  accent-color="#14B8A6"
+  button-base-color="#2107f5"
+  button-accent-color="#ffffff"
+  radius="large"
+  size="full"
+  position="top-left"
+  main-label="chat WITH AI"
   start-button-text="Start"
   end-button-text="End Call"
   require-consent="true"
