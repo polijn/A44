076e3678c69c00fdd3464040c7a492bb9792ac4c
polijn <45722770+polijn@users.noreply.github.com>
2025-07-08 11:26:49 +0200

    Create +page.svelte

---
+++ src/routes/testing/+page.svelte
@@ -0,0 +1,12 @@
+<script
+  src="https://unpkg.com/@vapi-ai/client-sdk-react/dist/embed/widget.umd.js"
+  async
+  type="text/javascript"
+>
+</script>
+
+
+Hello there: 
+
+
+<vapi-widget assistant-id="07e86b06-989e-40da-b09f-acde9377d6b5" public-key="77df2139-6329-4f96-9b08-4394fbfb865f"></vapi-widget>
